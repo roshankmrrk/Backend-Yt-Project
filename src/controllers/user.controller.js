df1e1c2c2393c5cf6d4a85e1d7554e2b69a7c54c
Roshan Kumar Rauniyar <roshankumarrauniyarstudent@gmail.com>
2026-07-25 22:11:55 +0530

    Insure that coverimage is optional when we register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,7 +26,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //Check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+let coverImageLocalPath;
+
+if (
+  req.files &&
+  Array.isArray(req.files.coverImage) &&
+  req.files.coverImage.length > 0
+) {
+  coverImageLocalPath = req.files.coverImage[0].path;
+}
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar local path file is required");
